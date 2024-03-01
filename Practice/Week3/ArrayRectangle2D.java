2dde5249147ace4d13fbed1652ad1621a2400cfe
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-03-01 15:31:02 +0700

    first commit

--- Practice/Week3/ArrayRectangle2D.java
+++ Practice/Week3/ArrayRectangle2D.java
@@ -1,5 +1,25 @@
 package Week3;
 
 public class ArrayRectangle2D {
-    
+    public static void main(String[] args) {
+        
+        Rectangle2D[][] rectangle2DArray = new Rectangle2D[3][3];
+
+        rectangle2DArray[0][0] = new Rectangle2D(45, 80);
+        rectangle2DArray[0][1] = new Rectangle2D(35, 75);
+        rectangle2DArray[0][2] = new Rectangle2D(20, 60);
+        rectangle2DArray[1][0] = new Rectangle2D(100, 85);
+        rectangle2DArray[1][1] = new Rectangle2D(75, 55);
+        rectangle2DArray[1][2] = new Rectangle2D(25, 70);
+        rectangle2DArray[2][0] = new Rectangle2D(120, 10);
+        rectangle2DArray[2][0] = new Rectangle2D(95, 30);
+        rectangle2DArray[2][1] = new Rectangle2D(45, 110);
+        rectangle2DArray[2][2] = new Rectangle2D(40, 130);
+
+        for (int i = 0; i < rectangle2DArray.length; i++) {
+            for (int j = 0; j < rectangle2DArray.length; j++) {
+                System.out.println("Rectangle (" + i + "," + j + ") length: " + rectangle2DArray[i][j].length + ", width: " + rectangle2DArray[i][j].width);
+            }
+        }
+    }
 }
--- Practice/Week3/Rectangle2D.java
+++ Practice/Week3/Rectangle2D.java
@@ -1,5 +1,11 @@
 package Week3;
 
 public class Rectangle2D {
-    
+    public int length;
+    public int width;
+
+    public Rectangle2D(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
 }
