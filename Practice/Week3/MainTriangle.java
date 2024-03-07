dc42412b23e026709b76da17bcf643f0c3cb1295
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-03-07 21:53:52 +0700

    first commit

---
+++ Practice/Week3/MainTriangle.java
@@ -0,0 +1,16 @@
+package Week3;
+
+public class MainTriangle {
+    public static void main(String[] args) {
+        Triangle[] trArray = new Triangle[4];
+
+        trArray[0] = new Triangle(10, 4);
+        trArray[1] = new Triangle(20, 10);
+        trArray[2] = new Triangle(15, 6);
+        trArray[3] = new Triangle(25, 10);
+
+        for (int i = 0; i < trArray.length; i++) {
+            System.out.println("Triangle " + i + " - Area: " + trArray[i].countArea() + ", Perimeter: " + trArray[i].countPerimeter());
+        }
+    }
+}
