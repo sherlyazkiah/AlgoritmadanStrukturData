6a1af48cf9e40994307ff0e1fe6cabfb5ec8f384
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-03-07 22:04:00 +0700

    first commit

--- Practice/Week3/Sphere.java
+++ Practice/Week3/Sphere.java
@@ -3,7 +3,19 @@ package Week3;
 public class Sphere {
     int r;
 
+    public Sphere(int j) {
+        r = j;
+    }
+
     double calcVol() {
-        
+        double vol = 0;
+        vol = (4.0/3.0)*Math.PI*r*r*r;
+        return vol;
+    }
+
+    double calcSurface() {
+        double sur = 0;
+        sur = 4*Math.PI*r*r;
+        return sur;
     }
 }
--- Practice/Week3/SquarePyramid22.java
+++ Practice/Week3/SquarePyramid22.java
@@ -3,6 +3,11 @@ package Week3;
 public class SquarePyramid22 {
     int side, height;
 
+    public SquarePyramid22(int side, int height){
+        side = this.side;
+        height = this.height;
+    }
+
     double calcVol() {
         double vol = 0;
         vol = (1.0/3.0)*(side*side)*height;
@@ -13,7 +18,8 @@ public class SquarePyramid22 {
         double sur = 0;
         double baseArea = side*side;
         double hypotenuse = Math.sqrt((height*height)+(0.5*side*0.5*side));
-        double lsgtg = ;
+        double lsgtg = 0.5*side*hypotenuse;
+        sur = baseArea+4*lsgtg;
         return sur;
     }
 }
