13b6c249a0591c2bc100cb0d740ec00756a6cb3c
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-03-22 12:50:49 +0700

    first commit

---
+++ Practice/Week6/Student.java
@@ -0,0 +1,21 @@
+package Week6;
+
+public class Student {
+    String name;
+    int entranceYear, age;
+    double gpa;
+
+    Student(String n, int y, int a, double g) {
+        name = n;
+        entranceYear = y;
+        age = a;
+        gpa = g;
+    }
+
+    void print() {
+        System.out.println("Nmae = " + name);
+        System.out.println("Entrance Year = " + entranceYear);
+        System.out.println("Age = " + age);
+        System.out.println("GPA = " + gpa);
+    }   
+}
---
+++ Practice/Week6/StudentList.java
@@ -0,0 +1,36 @@
+package Week6;
+
+public class StudentList {
+    Student list[] = new Student[5];
+    int idx;
+
+    void add(Student std) {
+        if (idx<list.length) {
+            list[idx] = std;
+            idx++;
+        } else {
+            System.out.println("The student list is already full-filled");
+        }
+    }
+
+    void print() {
+        for (Student s: list) {
+            s.print();
+            System.out.println("===============================");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < list.length-1; i++) {
+            for (int j = 0; j < list.length-i; j++) {
+                if (list[j].gpa > list[j-1].gpa) {
+                    //SWAP
+                    Student tmp = list[j];
+                    list[j] = list[j-1];
+                    list[j-1] = tmp;
+                }
+            }
+            
+        }
+    }
+}
