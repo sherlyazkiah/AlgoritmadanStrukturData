c0b0da720724beb187a05f668b5e85f71a94a6f4
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-05-08 23:36:26 +0700

    modify

--- Practice/Week10/PassengersMain.java
+++ Practice/Week10/PassengersMain.java
@@ -17,7 +17,7 @@ public class PassengersMain {
         System.out.print("Insert maximum queue : ");
         int max = sc.nextInt();
 
-        Queue Q = new Queue(max);
+        QueuePassengers queuePassenger = new QueuePassengers(max);
 
         int choose;
         do {
@@ -25,26 +25,38 @@ public class PassengersMain {
             choose = sc.nextInt();
             switch (choose) {
                 case 1:
+                    sc.nextLine();
                     System.out.print("Name: ");
-                    int newData = sc.nextInt();
-                    Q.Enqueue(newData);
+                    String nm = sc.nextLine();
+                    System.out.print("City Origin: ");
+                    String cOrg = sc.nextLine();
+                    System.out.print("City Destination: ");
+                    String cDes= sc.nextLine();
+                    System.out.print("Ticket Amount: ");
+                    int ticket = sc.nextInt();
+                    System.out.print("Price: ");
+                    int price = sc.nextInt();
+                    Passengers p = new Passengers(nm, cOrg, cDes, ticket, price);
+                    sc.nextLine();
+                    queuePassenger.Enqueue(p);
                     break;
                 case 2:
-                    int removeData = Q.Dequeue();
-                    if (removeData != 0) {
-                        System.out.println("Data removed : " + removeData);
+                    Passengers data = queuePassenger.Dequeue();
+                    if (!"".equals(data.name) && !"".equals(data.cityOrigin) &&
+                            !"".equals(data.cityDestination) && !"".equals(data.ticketAmount)
+                            && !"".equals(data.price)) {
+                        System.out.println("Data removed : " + data.name + " " + data.cityOrigin
+                        + " " + data.cityDestination + " " + data.ticketAmount + " " + data.price);
                         break;
                     }
                 case 3:
-                    Q.print();
+                    queuePassenger.peek();
                     break;
                 case 4:
-                    Q.peek();
+                    queuePassenger.print();
                     break;
-                case 5:
-                    Q.clear();
             }
-        } while (choose <= 5 && choose >=1);
+        } while (choose <= 4 && choose >=1);
 
         sc.close();
     }
---
+++ Practice/Week10/QueuePassengers.java
@@ -0,0 +1,109 @@
+package Week10;
+
+public class QueuePassengers {
+    
+    int max, size, front, rear;
+    Passengers[] Q;
+
+    public QueuePassengers(int n) {
+        max = n;
+        Create();
+    }
+
+    public void Create() {
+        Q = new Passengers[max];
+        size = 0;
+        front = rear = -1;
+    }
+
+    public boolean IsEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean IsFull() {
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void peek() {
+        if (!IsEmpty()) {
+            System.out.println("The first element : " + Q[front].name + " "
+            + Q[front].cityOrigin + " " + Q[front].cityDestination + " " +
+            Q[front].ticketAmount + " " + Q[front].price);
+        } else {
+            System.out.println("Queue is still empty");
+        }
+    }
+
+    public void print() {
+        if (!IsEmpty()) {
+            System.out.println("Queue is still empty");
+        } else {
+            int i = front;
+            while (i != rear) {
+                System.out.println("The first element : " + Q[front].name + " "
+            + Q[front].cityOrigin + " " + Q[front].cityDestination + " " +
+            Q[front].ticketAmount + " " + Q[front].price);
+                i = (i+1) % max;
+                
+            }
+            System.out.println(Q[i] + " ");
+            System.out.println("Element amount : " + size);
+        }
+    }
+
+    public void clear() {
+        if (!IsEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Queue has been cleared successfully");
+        } else {
+            System.out.println("Queue is still empty");
+        }
+    }
+
+    public void Enqueue(Passengers data) {
+        if (IsFull()) {
+            System.out.println("Queue is already full");
+        } else {
+            if (IsEmpty()) {
+                front = rear = 0;
+            } else {
+                if (rear == max - 1) {
+                    rear = 0;
+                } else {
+                    rear++;
+                }
+            }
+            Q[rear] = data;
+            size++;
+        }
+    }
+
+    public Passengers Dequeue() {
+        Passengers data = new Passengers("", "", "", 0, 0);
+        if (IsEmpty()) {
+            System.out.println("Queue is still empty");
+        } else {
+            data = Q[front];
+            size--;
+            if (IsEmpty()) {
+                front = rear = -1;
+            } else {
+                if (front == max - 1) {
+                    front = 0;
+                } else {
+                    front++;
+                }
+            }
+        }
+        return data;
+    }
+}
