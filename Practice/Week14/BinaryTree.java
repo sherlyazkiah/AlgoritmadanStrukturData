405160acd1983965d58c19f5fa65195c2ab28d4a
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-06-27 13:04:23 +0700

    feat: contLeaves method

--- Practice/Week14/BinaryTree.java
+++ Practice/Week14/BinaryTree.java
@@ -97,6 +97,16 @@ public class BinaryTree {
         printLeafNodes(node.right);
     }
 
+    int countLeaves(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == null && node.right == null) {
+            return 1;
+        }
+        return countLeaves(node.left) + countLeaves(node.right);
+    }
+    
     void traversePreOrder(Node node) {
         if (node != null) {
             System.out.println(" " + node.data);
